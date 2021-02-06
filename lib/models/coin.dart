51fab3368d591f9a138c56b69ff81ae2eec74380
Olayemii <donyemisco@gmail.com>
2021-02-06 22:34:12 +0100

    create model

--- lib/models/coin.dart
+++ lib/models/coin.dart
@@ -1,5 +1,23 @@
-enum CoinType {}
+enum CoinType {
+  BITCOIN,
+  ETHEREUM,
+  LITECOIN,
+  MONERO,
+  TRON,
+}
 
 class Coin {
   final CoinType type;
+  final double currentPrice;
+  final double percentProgress;
+  final double amountProgress;
+  final double balance;
+
+  Coin({
+    this.currentPrice,
+    this.percentProgress,
+    this.amountProgress,
+    this.balance,
+    this.type,
+  });
 }
