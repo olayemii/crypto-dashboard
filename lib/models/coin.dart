2e205694c904bc4250185aba99763bf5cd0b45dd
Olayemii <donyemisco@gmail.com>
2021-02-06 23:09:53 +0100

    fix images

--- lib/models/coin.dart
+++ lib/models/coin.dart
@@ -20,4 +20,36 @@ class Coin {
     this.balance,
     this.type,
   });
+
+  String getCoinAbbr() {
+    if (this.type == CoinType.BITCOIN) {
+      return "BTC";
+    } else if (this.type == CoinType.ETHEREUM) {
+      return "ETH";
+    } else if (this.type == CoinType.LITECOIN) {
+      return "LTC";
+    } else if (this.type == CoinType.MONERO) {
+      return "XMR";
+    } else if (this.type == CoinType.TRON) {
+      return "TRX";
+    } else {
+      return "";
+    }
+  }
+
+  String getImagePath() {
+    if (this.type == CoinType.BITCOIN) {
+      return "assets/images/bitcoin.png";
+    } else if (this.type == CoinType.ETHEREUM) {
+      return "assets/images/ethereum.png";
+    } else if (this.type == CoinType.LITECOIN) {
+      return "assets/images/litecoin.png";
+    } else if (this.type == CoinType.MONERO) {
+      return "assets/images/monero.png";
+    } else if (this.type == CoinType.TRON) {
+      return "assets/images/tron.png";
+    } else {
+      return "assets/images/bitcoin.png";
+    }
+  }
 }
