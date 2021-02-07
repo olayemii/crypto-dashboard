1947833941d7f6a06184b69004659eee546b2017
Olayemii <donyemisco@gmail.com>
2021-02-07 13:10:37 +0100

    begin coin detail fix

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
               color: kPrimaryColor,
             ),
           ),
-          initialRoute: "/",
+          initialRoute: "/coin-detail",
           onGenerateRoute: _onGenerateRoute,
         );
       },
