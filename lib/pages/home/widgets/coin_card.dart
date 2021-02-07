e2004b480b65968798d47c5fea2f62cab3484630
Olayemii <donyemisco@gmail.com>
2021-02-07 13:03:33 +0100

    secondary action

--- lib/pages/home/widgets/coin_card.dart
+++ lib/pages/home/widgets/coin_card.dart
@@ -1,7 +1,9 @@
 import 'package:crypto_dashboard/models/coin.dart';
+import 'package:crypto_dashboard/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:crypto_dashboard/utils/adaptive_height_extension.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class CoinCard extends StatelessWidget {
   final Coin coin;
@@ -9,95 +11,115 @@ class CoinCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeData themeData = Theme.of(context);
-    return Container(
-      height: 90.0.h,
-      color: Color.fromRGBO(55, 66, 92, 0.4),
-      padding: EdgeInsets.symmetric(
-        horizontal: 16.0,
-      ),
-      child: Row(
-        children: [
-          Container(
-            width: 40.0.w,
-            child: Image.asset(
-              coin.getImagePath(),
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.25,
+      child: Container(
+        height: 90.0.h,
+        color: Color.fromRGBO(55, 66, 92, 0.4),
+        padding: EdgeInsets.symmetric(
+          horizontal: 16.0,
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: 40.0.w,
+              child: Image.asset(
+                coin.getImagePath(),
+              ),
             ),
-          ),
-          SizedBox(
-            width: 20.0,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                "${coin.toString()} (${coin.getCoinAbbr()})",
-                                style: TextStyle(
-                                  color: Colors.white,
+            SizedBox(
+              width: 20.0,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  "${coin.toString()} (${coin.getCoinAbbr()})",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
                                 ),
-                              ),
-                              Spacer(),
-                              Text(
-                                "\$${coin.currentPrice}",
-                                style: TextStyle(
-                                  color: Colors.white,
+                                Spacer(),
+                                Text(
+                                  "\$${coin.currentPrice}",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 5.0,
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                "${coin.balance}",
-                                style: themeData.textTheme.caption
-                                    .copyWith(fontSize: 14.0),
-                              ),
-                              Spacer(),
-                              Row(
-                                children: [
-                                  Text(
-                                    getCoinProgress(coin),
-                                    style: TextStyle(
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 5.0,
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "${coin.balance}",
+                                  style: themeData.textTheme.caption
+                                      .copyWith(fontSize: 14.0),
+                                ),
+                                Spacer(),
+                                Row(
+                                  children: [
+                                    Text(
+                                      getCoinProgress(coin),
+                                      style: TextStyle(
+                                        color: coin.trend == Trend.UP
+                                            ? Color.fromRGBO(97, 201, 200, 1)
+                                            : Color.fromRGBO(247, 71, 104, 1),
+                                        fontSize: 13.0,
+                                      ),
+                                    ),
+                                    Icon(
+                                      coin.trend == Trend.UP
+                                          ? FlutterIcons.caret_up_faw
+                                          : FlutterIcons.caret_down_faw,
+                                      size: 14.0,
                                       color: coin.trend == Trend.UP
                                           ? Color.fromRGBO(97, 201, 200, 1)
                                           : Color.fromRGBO(247, 71, 104, 1),
-                                      fontSize: 13.0,
-                                    ),
-                                  ),
-                                  Icon(
-                                    coin.trend == Trend.UP
-                                        ? FlutterIcons.caret_up_faw
-                                        : FlutterIcons.caret_down_faw,
-                                    size: 14.0,
-                                    color: Color.fromRGBO(97, 201, 200, 1),
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
+      actions: <Widget>[
+        IconSlideAction(
+          color: kDangerColor,
+          icon: FlutterIcons.x_fea,
+          onTap: () => null,
+        ),
+      ],
+      secondaryActions: <Widget>[
+        IconSlideAction(
+          color: kDangerColor,
+          icon: FlutterIcons.x_fea,
+          onTap: () => null,
+        ),
+      ],
     );
   }
 }
